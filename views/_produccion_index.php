fc5a5fba3006c1cb8d838ae74a65edd2ff9f8b92
Brandon Parrillas <brandonps.dev@gmail.com>
2023-05-16 10:12:44 -0600

    se regreso al modo desarrollo

--- views/_produccion_index.php
+++ views/_produccion_index.php
@@ -5,8 +5,8 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Reservaciones</title>
-  <script type="module" crossorigin src="/reservaciones/views/assets/index-b8ecd4a0.js"></script>
-  <link rel="stylesheet" href="/reservaciones/views/assets/index-16134a42.css">
+  <script type="module" crossorigin src="/reservaciones/views/assets/index.js"></script>
+  <link rel="stylesheet" href="/reservaciones/views/assets/index.css">
 </head>
 
 <body>
